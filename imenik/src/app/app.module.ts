c8ffd2bdb475639fdde37ca9e5743438e357b124
matpav1993 <matea.pavic1993@gmail.com>
2021-05-26 10:15:11 +0200

    Routing, Comp(home, login,register),formatiranje

--- imenik/src/app/app.module.ts
+++ imenik/src/app/app.module.ts
@@ -6,13 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { LoginComponent } from './components/account/login/login.component';
-import { RegisterComponent } from './components/account/register/register.component';
 import { DodajKontaktComponent } from './components/imenik/dodaj-kontakt/dodaj-kontakt.component';
 import { PopisKontaktaComponent } from './components/imenik/popis-kontakta/popis-kontakta.component';
 import { UrediKontaktComponent } from './components/imenik/uredi-kontakt/uredi-kontakt.component';
 import { DetaljiKontaktaComponent } from './components/imenik/detalji-kontakta/detalji-kontakta.component';
-import { HomeComponent } from './components/imenik/home/home.component';
 import { NavigationComponent } from './components/shared/navigation/navigation.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { FormsModule } from '@angular/forms';
@@ -21,13 +18,10 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     DodajKontaktComponent,
     PopisKontaktaComponent,
     UrediKontaktComponent,
     DetaljiKontaktaComponent,
-    HomeComponent,
     NavigationComponent,
     BackButtonComponent
   ],
