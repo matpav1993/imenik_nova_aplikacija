5257cd9c1ddd3212b6f5f6658173fef8e7ff5f8f
matpav1993 <matea.pavic1993@gmail.com>
2021-06-21 16:25:42 +0200

    suffix, font, menu button, css

--- imenik/src/app/components/imenik/detalji-kontakta/detalji-kontakta.component.ts
+++ imenik/src/app/components/imenik/detalji-kontakta/detalji-kontakta.component.ts
@@ -50,15 +50,17 @@ export class DetaljiKontaktaComponent implements OnInit {
     this.router.navigate(['/uredi/', this.kontaktID]);
   }
 
- async asyncBtnBrisanje(id2: number): Promise<void> {
+  async asyncBtnBrisanje(id2: number): Promise<void> {
     const dialogRef = this.dialog.open(DialogBrisanjeComponent, {
       data: { id: id2, title: 'Jeste li sigurni da želite obrisati ovaj kontakt?' }
     });
-    const potvrda = await  dialogRef.afterClosed().toPromise();
-    if (potvrda){
-     await this.apiService.deleteEmail(this.kontaktID).toPromise();
-     await this.apiService.deleteTelefon(this.kontaktID).toPromise();
-     await this.apiService.deleteKontakt(this.kontaktID).toPromise();
-     this.router.navigate(['/']);
+    const potvrda = await dialogRef.afterClosed().toPromise();
+    if (potvrda) {
+      await this.apiService.deleteEmail(this.kontaktID).toPromise();
+      await this.apiService.deleteTelefon(this.kontaktID).toPromise();
+      await this.apiService.deleteKontakt(this.kontaktID).toPromise();
+      this.router.navigate(['/']);
+    }
   }
-}}
+}
+
